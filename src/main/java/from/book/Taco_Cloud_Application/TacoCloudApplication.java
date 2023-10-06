f280a46e55ea9d71b0a6c5163c6a53a4c5cccf51
piotru <ania@op.pl>
2023-10-06 18:46:45 +0200

    Start Security Taco Claud of application

---
+++ src/main/java/from/book/Taco_Cloud_Application/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package from.book.Taco_Cloud_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
